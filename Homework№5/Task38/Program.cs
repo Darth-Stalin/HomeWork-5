d4c3e2c9711cea94776b468beca5acfdc904f2aa
Darth_Stalin <cusima123@mail.ru>
2023-06-06 18:22:31 +0900

    Task36

---
+++ Homework№5/Task38/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.\
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] randomArray = new double[size];
+ArrayRandomNumbers(randomArray);
+Console.WriteLine("Наш массив: ");
+PrintArray(randomArray);
+double razn = 0;
+double max = randomArray[0];
+double min = randomArray[0];
+
+for (int i = 0; i < size; i++)
+{
+    if(min > randomArray[i])
+    min = randomArray[i];
+    if(max < randomArray[i])
+    max = randomArray[i];
+
+    razn = max - min;
+}
+Console.WriteLine($"Разница между максимальным и минимальным значением: {razn}");
+
+void ArrayRandomNumbers(double[] randomArray)
+{
+    Random rand = new Random();
+for (int i = 0; i < size; i++)
+    {
+        randomArray[i] = rand.NextDouble();
+    }
+}
+
+void PrintArray(double[] randomArray)
+{
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write(randomArray[i] + " ");
+    }
+    Console.WriteLine();
+}
